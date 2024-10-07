f7b938575ba2b2decd84e13376c6ee8e832fefca
Olga_N <nazar.olgaa@gmail.com>
2024-10-07 14:27:56 -0400

    Initial Commit

---
+++ MovieWatchList/MovieWatchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieWatchListApp.swift
+//  MovieWatchList
+//
+//  Created by Sergey Nazarov on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieWatchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieWatchListTests/MovieWatchListTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieWatchListTests.swift
+//  MovieWatchListTests
+//
+//  Created by Sergey Nazarov on 10/7/24.
+//
+
+import Testing
+@testable import MovieWatchList
+
+struct MovieWatchListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
